ef010550680719cc7d3741505976df98229b199c
lies046 <skytree.skysky@gmail.com>
2020-01-21 15:47:01 +0900

    修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   validates :title, :content, presence: true
   belongs_to :user
   has_many :likes, dependent: :destroy
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
 
   def like_user(user_id)
     likes.find_by(user_id: user_id)

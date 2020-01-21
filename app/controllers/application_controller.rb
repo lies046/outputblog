85aff0a024c2317789c6ffccfd08c8faca828d1a
lies046 <skytree.skysky@gmail.com>
2020-01-21 20:01:21 +0900

    ベーシック認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
      posts_path # ログイン後に遷移するpathを設定
@@ -14,5 +16,17 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image])
 
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :image])
+  end
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end

9db81192c4a7e836b03d43571be72a7c3c1e48c3
lies046 <skytree.skysky@gmail.com>
2020-01-21 15:40:19 +0900

    ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'top#index'
+  resources :posts do
+    resources :likes, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end

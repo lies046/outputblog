177b6fdf58b5cba8cc64af0377932c4c54852829
lies046 <skytree.skysky@gmail.com>
2020-01-21 17:52:18 +0900

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   mount_uploader :image, ImagesUploader
   validates :title, :content, presence: true
   belongs_to :user
-  has_many :likes, dependent: :destroy
+  # has_many :likes, dependent: :destroy
   
   def like_user(user_id)
     likes.find_by(user_id: user_id)

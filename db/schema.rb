6172bbdb821fc79cf6fda666f1c00cdff7f4e1c6
lies046 <skytree.skysky@gmail.com>
2020-01-22 17:45:20 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_01_22_043320) do
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
-    t.text "text"
+    t.text "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

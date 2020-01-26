57414e5d87da7874032c83ea517111f0480dd4d2
lies046 <skytree.skysky@gmail.com>
2020-01-26 18:18:57 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
      root_path # ログアウト後に遷移するpathを設定
   end
-
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image])
@@ -18,7 +18,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-
   def production?
     Rails.env.production?
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    sessions: 'users/sessions' } 
   root to: 'top#index'
   resources :posts do
     resources :likes, only: [:create, :destroy]

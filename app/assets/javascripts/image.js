85b6df6d8012f45c1c42a432e7002ffb269d9756
lies046 <skytree.skysky@gmail.com>
2020-01-26 16:22:44 +0900

    修正

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -4,7 +4,7 @@ $(function() {
       if (input.files && input.files[0]) {
       var reader = new FileReader();
       reader.onload = function (e) {
-  $('#img_prev').attr('src', e.target.result);
+  $('.icon-image').attr('src', e.target.result);
       }
       reader.readAsDataURL(input.files[0]);
       }
@@ -14,6 +14,20 @@ $(function() {
   });
 });
 
+$(function() {
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+  $('.icon').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+      }
+  }
+  $("#user_img").change(function(){
+      readURL(this);
+  });
+});
 
 $(function(){
   $fileField = $('#file')

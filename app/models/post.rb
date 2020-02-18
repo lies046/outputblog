441626ad792680b30da1989a50b98ba6fa01f41f
lies046 <skytree.skysky@gmail.com>
2020-02-18 14:05:06 +0900

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  
+
   def like_user(user_id)
     likes.find_by(user_id: user_id)
   end

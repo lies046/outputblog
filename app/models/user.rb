06db41304a51da41ad0981966ba922e391309d20
lies046 <skytree.skysky@gmail.com>
2020-02-21 21:14:48 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,33 @@
 class User < ApplicationRecord
+    def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        id: User.all.last.id + 1,
+        uid:      auth.uid,
+        provider: auth.provider,
+        nickname:  auth.info.name,
+        image: auth.info.image,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
+
   mount_uploader :image, ImagesUploader
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        
-         has_many :likes, dependent: :destroy
+         :recoverable, :rememberable, :validatable,:omniauthable,omniauth_providers: [:twitter]
+  has_many :likes, dependent: :destroy
          has_many :posts, dependent: :destroy
          has_many :comments,dependent: :destroy
-         validates :nickname, presence: true,uniqueness: true, length: {in:3..6}
+         validates :nickname, presence: true,uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations',
-                                    sessions: 'users/sessions' } 
+                                    sessions: 'users/sessions',
+                                    omniauth_callbacks: 'users/omniauth_callbacks'} 
   root to: 'top#index'
   resources :posts do
     resources :likes, only: [:create, :destroy]
       resources :comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
+ 
 end
